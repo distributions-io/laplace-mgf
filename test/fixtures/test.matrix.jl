3cb2787c66c3c0be64cdb622f4d5fe1e246bad41
Planeshifter <pburckhardt@outlook.com>
2015-08-07 20:25:44 +0200

    [INIT]

---
+++ test/fixtures/test.matrix.jl
@@ -0,0 +1,24 @@
+using Distributions
+using JSON
+
+mu = 0
+b = 1
+d = Laplace( mu,b )
+
+x = linspace( 1, 0, 25 )
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("mu", mu),
+	("b", b),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/matrix.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
---
+++ test/fixtures/test.number.jl
@@ -0,0 +1,24 @@
+using Distributions
+using JSON
+
+mu = 0
+b = 1
+d = Laplace( mu,b )
+
+x = [ -5, -2.5, 0, 2.5, 5 ]
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("mu", mu),
+	("b", b),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/number.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
